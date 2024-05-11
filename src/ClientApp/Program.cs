f7a603237f3d4ec917436740393748fa89daef54
João Antunes <joao@antunes.dev>
2024-05-11 18:21:41 +0100

    Adding logging handler to traditional HTTP requests as well

--- src/ClientApp/Program.cs
+++ src/ClientApp/Program.cs
@@ -4,16 +4,17 @@ using Grpc.Core.Interceptors;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient();
+builder.Services
+    .AddHttpClient(nameof(SomeBackgroundService), o => o.BaseAddress = new Uri("https://localhost:7070"))
+    .AddHttpMessageHandler<SomeDelegatingHandler>();
 builder.Services.AddHostedService<SomeBackgroundService>();
 builder.Services
     .AddGrpcClient<Greeter.GreeterClient>(o => o.Address = new Uri("https://localhost:7070"))
     .AddInterceptor<SomeGrpcInterceptor>()
     .AddHttpMessageHandler<SomeDelegatingHandler>();
 
-builder.Services
-    .AddSingleton<SomeGrpcInterceptor>()
-    .AddSingleton<SomeDelegatingHandler>();
+builder.Services.AddSingleton<SomeGrpcInterceptor>();
+builder.Services.AddTransient<SomeDelegatingHandler>();
 
 var app = builder.Build();
 
@@ -32,24 +33,26 @@ app.MapGet("/", async (HttpContext ctx,HttpClient client, ILogger<Program> logge
 
 app.Run();
 
-public class SomeBackgroundService(IServiceScopeFactory scopeFactory, ILogger<SomeBackgroundService> logger)
+public class SomeBackgroundService(
+    IHttpClientFactory clientFactory,
+    IServiceScopeFactory scopeFactory, 
+    ILogger<SomeBackgroundService> logger)
     : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("activity id is {ActivityId}", Activity.Current?.Id ?? "null");
 
-        using var scope = scopeFactory.CreateScope();
-
         // out of the box, will hit the server app, not including the traceparent header (because there's no activity in this scope)
         // so the server app will send the trace to the collector
-        var client = scope.ServiceProvider.GetRequiredService<HttpClient>();
+        var client = clientFactory.CreateClient(nameof(SomeBackgroundService));
         _ = await client.GetAsync("https://localhost:7070", stoppingToken);
 
         // out of the box, will hit the server app, including the traceparent header with sampled flag set to false
         // so the server app will not send the trace to the collector (unless we force always sampling on the server app)
-        // I assume the traceparent header is sent in this case and not the above, because somewhere along the way
+        // the traceparent header is sent in this case and not the above, because somewhere along the way
         // some activity is created
+        using var scope = scopeFactory.CreateScope();
         var grpcClient = scope.ServiceProvider.GetRequiredService<Greeter.GreeterClient>();
         _ = await grpcClient.SayHelloAsync(new HelloRequest { Name = "Tester" }, cancellationToken: stoppingToken);
     }
